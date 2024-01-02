f94aea60700a838a2075fa3fe4c3e3cc0752cabe
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-02 21:35:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "6954351996:AAH6yn9Q7I10gNV_BnAnXjfZckJbrAXAe84"
 
 # Project Name of your Project
 PROJECT_NAME = "GitGram"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/mahtoanjali/Anjali-Api-Id"
